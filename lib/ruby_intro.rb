bb5b4e13c9e98b0528616b6c1df8d8d7951fbdf9
mymaymy131145 <mymaymy131145@gmail.com>
2022-12-23 13:51:36 +0700

    Part2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -11,21 +11,25 @@ def max_2_sum arr
 end
 
 def sum_to_n? arr, n
-  
+  arr.product(arr).any? {|couple| sum(couple) == n} 
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s =~  /^[fhdhdhfgjghgdsf]/i 
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if bin = /^[1|0]+/.match(s) 
+    bin.to_s.to_i(2) % 4 == 0
+  else
+   false
+  end
 end
 
 # Part 3
