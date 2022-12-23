27de5d10e5cd77895f72a4b1654e3a1dcec07b38
mymaymy131145 <mymaymy131145@gmail.com>
2022-12-23 13:54:07 +0700

    Part3

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -34,6 +34,21 @@ end
 
 # Part 3
 
-class BookInStock
-# YOUR CODE HERE
+class BookInStock   
+    attr_accessor :isbn, :price
+    
+    def initialize(new_isbn, new_price)
+      if new_isbn == ''
+        raise ArgumentError.new("Must have an ISBN number") 
+      end
+      self.isbn = new_isbn
+      if new_price <= 0
+        raise ArgumentError.new("Must have a positive price")
+      end
+      self.price = new_price.to_f 
+    end
+    #toString
+    def price_as_string
+      return sprintf "$"+"%.2f", price 
+    end
 end
